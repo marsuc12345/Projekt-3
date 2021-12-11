789d2c5824aaab9376c7fdecf1a3d009b4a9e8e8
marsus12345 <marsuc@seznam.cz>
2021-12-11 19:02:18 +0100

    Projekt 3

--- projekt 3 elections scraper.py
+++ projekt 3 elections scraper.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
-import pprint
+import csv
+
 
 url = "https://volby.cz/pls/ps2021/ps32?xjazyk=CZ&xkraj=14&xnumnuts=8103"
-vystup = ""
+vystup = "pokus.csv"
 
-# faze 1 v linku nize skrejpovat LINK, CISLO a NAZEV obci do slovniku
+
+# faze 1 z linku vyse skrejpovat LINK, NAZEV a CISLO obci do listu
 
 req_obce = requests.get(url)
 soup_obce = bs(req_obce.text, "html.parser")
@@ -16,29 +18,30 @@ for a_elem in soup_obce.find_all("a"):
     href = urljoin(url, a_elem["href"])
     if len(href) > 70 and href not in nalezene_odkazy:
         nalezene_odkazy.append(href)
-# pprint.pprint(nalezene_odkazy)
 
 nalezene_obce = []
 for elem in soup_obce.find_all("td",{"class":"overflow_name"}):
     text = elem.text
     nalezene_obce.append(text)
-# print(nalezene_obce)
 
 nalezene_cisla = []
-for znak in nalezene_odkazy:
-    nalezene_cisla.append(znak[59:65])              # funkcnejsi skrejp pro jine okrsky - napr. u prahy to nefachci
-# print(nalezene_cisla)
+for elem in soup_obce.find_all("td",{"headers":["t1sb1","t2sb1","t3sb1"]}):
+    text = elem.text
+    nalezene_cisla.append(text)
 
 
-# faze 2 skrejpovat hlavicku a vsechny potrebne udaje z linku vsech obci + ulozeni do .csv
+# faze 2 skrejpovat HLAVICKU a potrebne NALEZENE udaje a ulozit vse do .CSV
 
 hlavicka = ["kód obce","název obce","voliči v seznamu","vydané obálky","platné hlasy"]
 req_hlavicka = requests.get(nalezene_odkazy[0])
 soup_hlavicka = bs(req_hlavicka.text, "html.parser")
-for prvek in soup_hlavicka.find_all("td", {"headers": ["t1sa1 t1sb2","t2sa1 t2sb2"]}):
+for prvek in soup_hlavicka.find_all("td", {"headers": ["t1sb2","t2sb2"]}):
     nadpis = prvek.text
     hlavicka.append(nadpis)
-print(hlavicka[:-1])                                # pridat hlavicku do .csv !!!
+print(hlavicka)
+f = open(vystup, "w", encoding="utf-8", newline="")
+f_writer = csv.writer(f)
+f_writer.writerow(hlavicka)
 
 nalezene_udaje = []
 index = 0
@@ -48,10 +51,10 @@ for link in nalezene_odkazy:
     nalezene_udaje.append(nalezene_cisla[index])
     nalezene_udaje.append(nalezene_obce[index])
     index += 1
-    for elem in soup_detaily.find_all("td",{"headers":["sa2","sa3","sa6","t1sa2 t1sb3","t2sa2 t2sb3"]}):
+    for elem in soup_detaily.find_all("td",{"headers":["sa2","sa3","sa6","t1sb3","t2sb3"]}):
         text = elem.text
         nalezene_udaje.append(text)
-    print(nalezene_udaje[:-1])                      # pridat tisk radku do .csv !!!
-    break                                           # STOP pro prvni obec kvuli delce procesu !!!
-
-
+    f_writer.writerow(nalezene_udaje)
+    print(nalezene_udaje)
+    nalezene_udaje = []
+f.close()
